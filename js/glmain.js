c3c97bab33c7dae7fbbb1dcaeb362f98b6b88104
Aditya Mattoo <aditya.mattoo88@gmail.com>
2017-05-08 21:38:31 -0600

    gl_init

--- js/glmain.js
+++ js/glmain.js
@@ -1,12 +1,39 @@
-﻿(function () {
-    
-    var sr = this;
+﻿(function (){
+   angular
+       .module('app', []);
 
-    function glInit(){
-       
-        sr.canvas = document.getElementById(canvas);
-        sr.gl = canvas.getContext(webgl);
+   app.glInit();
+   app.draw();
 
+
+    app.glInit() = function () {
+        var that = this;
+
+        canvas = document.getElementById("canvas");
+        app.gl = canvas.getContext("webgl");
+        app.gl.viewport(0, 0, canvas.clientWidth, canvas.clientHeight);
+        app.gl.clearcolor(1, 1, 0, 1);
     }
 
+        app.draw() = function () {
+
+        app.gl.clear(gl.COLOR_BUFFER_BIT);
+    }
 })
+
+
+
+initGL();
+draw();
+var gl;
+
+function initGL() {
+    var canvas = document.getElementById("canvas");
+    gl = canvas.getContext("webgl");
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1, .5, 0, 1);
+}
+
+function draw() {
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
